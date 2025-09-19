d710e8713708dc5c1b1b6c948d0db054dd00e0eb
SemkiShow <mgdeveloper123@gmail.com>
2025-09-19 19:07:12 +0200

    Update files

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 set -e
+executable_name=SampleRaylibProject
 
 # Release build
 if [ "$1" == "" ]; then
     clear
     ./reset_save_files.sh --soft
-    cmake -B build
-    cmake --build build -j ${nproc}
-    ./build/bin/main
+    cmake -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    cmake --build build -j32
+    ./build/bin/$executable_name
 fi
 
 # Debug build
@@ -16,12 +17,21 @@ if [ "$1" == "-d" ] || [ "$1" == "--debug" ]; then
     clear
     ./reset_save_files.sh --soft
     cmake -B build_debug -DCMAKE_BUILD_TYPE=Debug
-    cmake --build build_debug -j ${nproc}
-    gdb -ex run ./build_debug/bin/main
+    cmake --build build_debug -j32
+    gdb -ex run --args ./build_debug/bin/$executable_name
+fi
+
+# Windows build
+if [ "$1" == "-w" ] || [ "$1" == "--windows" ]; then
+    clear
+    ./reset_save_files.sh --soft
+    cmake -B build_win -DCMAKE_TOOLCHAIN_FILE="$(pwd)/mingw-w64-x86_64.cmake" -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    cmake --build build_win -j32
+    wine ./build_win/bin/$executable_name.exe
 fi
 
 # Web build
-if [ "$1" == "-w" ] || [ "$1" == "--web" ]; then
+if [ "$1" == "--web" ]; then
     clear
     ./reset_save_files.sh --soft
     if [ "$2" == "-m" ] || [ "$2" == "--minimal" ]; then
@@ -30,7 +40,7 @@ if [ "$1" == "-w" ] || [ "$1" == "--web" ]; then
         emcmake cmake -B build_web -DPLATFORM=Web -DSHELL=Full
     fi
     cmake --build build_web -j ${nproc}
-    emrun ./build_web/bin/main.html
+    emrun ./build_web/bin/index.html
 fi
 
 # Help info
@@ -40,7 +50,7 @@ if [ "$1" == "--help" ]; then
     echo ""
     echo "With no OPTION, compile and run the release build"
     echo ""
-    echo "-d, --debug    Compile the debug build and run it with gdb"
-    echo "-w, --web      Compile the WebAssembly build"
-    echo "-m, --minimal  Compile a WebAssembly build with the minimal shell"
+    echo "-d, --debug      Compile the debug build and run it with gdb"
+    echo "-w, --windows    Compile the Windows build and run it with Wine"
+    echo "--web            Compile the web build and run it with emrun"
 fi
