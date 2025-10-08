da3d5f67093cb9aab7e203985d5c7286a1105381
SemkiShow <mgdeveloper123@gmail.com>
2025-10-08 18:53:00 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
 set -e
+executable_name=SampleRaylibProject
 
 # Compiling for Linux
-if [ ! -d build ]; then
-    mkdir build
-fi
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-cd ..
-cp build/bin/main main
+cmake -B build_release -DCMAKE_BUILD_TYPE=Release
+cmake --build build_release -j
+cp build_release/bin/$executable_name .
 
 # Compiling for Windows
-if [ ! -d build_win ]; then
-    mkdir build_win
-fi
-cd build_win
-cmake .. -DCMAKE_BUILD_TYPE=Release --toolchain ../mingw-w64-x86_64.cmake
-make
-cd ..
-cp build_win/bin/main.exe .
+cmake -B build_release_windows -DCMAKE_TOOLCHAIN_FILE=mingw-w64-x86_64.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build build_release_windows -j
+cp build_release_windows/bin/$executable_name.exe .
 
 # Zipping the build
-./reset_save_files.sh
-zip release.zip main main.exe assets/* LICENSE README.md settings.txt
-rm main main.exe
+zip release.zip $executable_name $executable_name.exe LICENSE README.md resources/**
+rm $executable_name $executable_name.exe
 
 # Creating a GitHub release
 gh release create $1 release.zip
